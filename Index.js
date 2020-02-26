d0fdfa8410bc5510472ddd729884902c10c7908e
Mr-mc <60752827+Mr-mc@users.noreply.github.com>
2020-02-25 18:19:21 -0600

    It might not be necessary but I wrote this to temporary store the User. When User = new User() the locastroage will change live. It only changes the code slightly.

--- Index.js
+++ Index.js
@@ -1,5 +1,13 @@
-var SaveNameButton = document.getElementById('button_name')
-SaveNameButton.addEventListener("click", function() {
+let SaveNameButton = document.getElementById('button_name');
+
+function User(){
+	this.name = SaveNameButton;
+};
+User = new User();
+User.name.addEventListener("click", function() {
 	var creat = document.getElementById('creator');
-		window.localStorage.setItem('user', creat.value);
+		window.localStorage.setItem('Creator', creat.value);
+
+	User.name = creat;
+	console.log(User.name);
 });
